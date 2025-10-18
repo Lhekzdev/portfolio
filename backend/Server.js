2885c36a3285c104b98a7026f8707f0e07b3f88d
Olaseinde Aduraleke <lhekzdev@gmail.com>
2025-10-18 13:21:45 +0100

    fix: bind server to 0.0.0.0 for Render

--- backend/Server.js
+++ backend/Server.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import uploadRoute from "./routes/uploadRoute.js";
 import contactRoutes from "./routes/Contact.js";
 
@@ -22,6 +25,21 @@ app.use(express.json());
 app.use("/api/contact", contactRoutes);
 app.use("/api/uploadRoute", uploadRoute);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
+// / ✅ Frontend build setup
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "../dist"))); // Vite default build folder
+// If you're using CRA: "../build"
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+})
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
