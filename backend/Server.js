0245fd614a821c576ad38309f2048997ab757029
Olaseinde Aduraleke <lhekzdev@gmail.com>
2025-10-22 14:44:06 +0100

    updated server.js

--- backend/Server.js
+++ backend/Server.js
@@ -12,10 +12,10 @@ dotenv.config();
 
 const MONGO_URI = process.env.MONGO_URI;
 
-mongoose
-  .connect(MONGO_URI)
-  .then(() => console.log("✅ Connected to MongoDB"))
-  .catch((err) => console.error("❌ Error connecting to MongoDB:", err));
+// mongoose
+//   .connect(MONGO_URI)
+//   .then(() => console.log("✅ Connected to MongoDB"))
+//   .catch((err) => console.error("❌ Error connecting to MongoDB:", err));
 
 const app = express();
 
